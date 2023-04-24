2427e52eeda5ed392e9dc5ec100c2431af3f0017
Mephir12 <piorunas10@gmail.com>
2023-04-24 13:15:53 +0200

    6. Zmiana literałów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int STARTING_TOTAL_REJECTED = 0;
+    int totalRejected = STARTING_TOTAL_REJECTED;
 
     public int reportRejected() {
         return totalRejected;
